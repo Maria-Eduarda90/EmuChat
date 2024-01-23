edc0e6f05bd6ba5a4d57632757eedc475c4710f1
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-01-23 12:49:56 -0300

    feat: mongodb configuration

---
+++ src/services/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    if (typeof process.env.MONGOOSE === "undefined") {
+      throw new Error("");
+    } else {
+      await mongoose.connect(process.env.MONGOOSE);
+    }
+  } catch (err) {
+    throw new Error(err as string).message;
+  }
+}
