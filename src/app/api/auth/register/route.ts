63e83b2a0f9603f5d164daf3e60dc7b2d957c3d4
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-01-23 12:49:19 -0300

    feat: api for creating the user with mongodb

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,48 @@
+import User from "@/schema/User";
+
+import { UserRegisterType } from "@/@types/UserRegister";
+import { connect } from "@/services/db";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: Request) {
+  try {
+    const { name, email, password } =
+      (await request.json()) as UserRegisterType;
+
+    await connect();
+
+    const emailExists = await User.findOne({
+      email,
+    });
+
+    if (emailExists) {
+      return NextResponse.json({
+        message: "E-mail já cadastrado",
+        status: 409,
+      });
+    }
+
+    console.log("api: ", emailExists);
+
+    const hashedPassword = await bcrypt.hash(password, 5);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    return NextResponse.json({
+      message: "Usuario criado com sucesso!",
+      status: 201,
+    });
+  } catch (err) {
+    return NextResponse.json({
+      err: "Erro ao cadastrar usuario",
+      status: 500,
+    });
+  }
+}
