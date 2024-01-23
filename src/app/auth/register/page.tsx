a9865a8f36e664f7cd6d16a59c3f9ad7e30d7a3d
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-01-23 13:57:53 -0300

    feat: import of the hook to use in the form

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
+import { useRegister } from '@/hooks/useRegister';
 import { initialValuesRegister } from '@/initialValuesFormik/initialValues.register';
 import { validationSchemaRegister } from '@/validation/register';
 
@@ -9,10 +10,11 @@ import { Form, Formik } from 'formik';
 import Link from 'next/link';
 
 export default function Register() {
-
-    function handleSubmit(e: any) {
-        console.log(e)
-    }
+    const {
+        error,
+        isFormSubmitting,
+        handleSubmit
+    } = useRegister();
 
     return (
         <main className='min-h-screen flex items-center justify-center'>
@@ -21,17 +23,25 @@ export default function Register() {
                 initialValues={initialValuesRegister}
                 validationSchema={validationSchemaRegister}
             >
-                {({ values }: any) => (
+                {({ values }) => (
                     <Form noValidate className='flex flex-col gap-2 p-4 border border-zinc-300 min-w-[400px] rounded'>
                         <Input name='name' type='text' required />
                         <Input name='email' type='email' required />
                         <Input name='password' type='password' required />
 
-                        <Button type='submit' text="Entrar" className='bg-teal-400 tex-white rounded p-2 cursor-pointer' />
+                        <Button
+                            type='submit'
+                            text={isFormSubmitting ? "Carregando" : "Inscreva-se"}
+                            className='bg-teal-400 tex-white rounded p-2 cursor-pointer'
+                            disabled={isFormSubmitting}
+                        />
+                        {error && (
+                            <span className="text-red-500 text-sm text-center">{error}</span>
+                        )}
                         <span className=' text-center text-zinc-200'>
                             Não possui uma conta?
                             <strong className='text-zinc-400 ml-1'>
-                                <Link href={'/login'}> Entre</Link>
+                                <Link href={'/auth/login'}> Entre</Link>
                             </strong>
                         </span>
                     </Form>
