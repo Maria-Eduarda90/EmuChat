71b7d1f5b18d9ef75eb210a8c3e9e91ff848908e
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-01-24 13:24:59 -0300

    feat: checking if my user is logged in, if not he returns to the login screen, this serves to protect the routes and only be able to access if he is logged in

---
+++ src/components/LayoutAdmin/index.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { ChildrenType } from "@/@types/ChildrenType";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export function LayoutAdmin({ children }: ChildrenType) {
+    const router = useRouter();
+    const { data: session, status } = useSession();
+
+    if (status === "loading") {
+        return null;
+    }
+
+    if (!session) {
+        setTimeout(() => {
+            router.push('/auth/login');
+        }, 100);
+
+        return null
+    }
+
+    return <div className="min-h-screen">{children}</div>
+}
