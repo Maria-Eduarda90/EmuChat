29453bc19c80281843329842e5f9a091e9000671
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-01-24 15:26:50 -0300

    feat: taking the logged in user section and putting the first name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+'use client'
+
+import { LayoutAdmin } from "@/components/LayoutAdmin";
+import { useSession } from "next-auth/react";
+
 export default function Home() {
+  const { status, data: session } = useSession();
+
   return (
-    <div>teste</div>
+    <LayoutAdmin>
+      <div>{session?.user?.name?.split(" ")[0]}</div>
+    </LayoutAdmin>
   );
 }
