3bd6d660126884ed816d4b8e4db287dec4fd4215
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-01-25 11:34:09 -0300

    feat: created component header

---
+++ src/components/HeaderSession/index.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/Button";
+import { signOut, useSession } from "next-auth/react";
+
+export function HeaderSession() {
+    const { status, data: session } = useSession();
+
+    return (
+        <div className="flex justify-end gap-4 mt-4 mr-4">
+            <div>Olá {`${session?.user?.name?.split(" ")[0]}`}</div>
+            <Button
+                text="sair"
+                className="bg-red-600 text-white rounded px-4 cursor-pointer"
+                onClick={() => signOut()}
+            />
+        </div>
+    );
+}
