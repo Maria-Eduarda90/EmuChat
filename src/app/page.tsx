08882d94d05b16d2c12ce47c78b47061efb25372
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-01-25 11:23:52 -0300

    feat: next-auth function for user to log out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 'use client'
 
+import { Button } from "@/components/Button";
 import { LayoutAdmin } from "@/components/LayoutAdmin";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 export default function Home() {
   const { status, data: session } = useSession();
 
   return (
     <LayoutAdmin>
-      <div>{session?.user?.name?.split(" ")[0]}</div>
+      <div className="flex justify-end gap-1">
+        <div>{session?.user?.name?.split(" ")[0]}</div>
+        <Button
+          text="sair"
+          className="bg-red-600 text-white rounded px-2 cursor-pointer"
+          onClick={() => signOut()}
+        />
+      </div>
     </LayoutAdmin>
   );
 }
