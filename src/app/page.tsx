7f1a43502740688f913ad6545a5cf580cadd5b6f
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-01-25 20:14:57 -0300

    refactor: removing unnecessary imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 'use client'
 
-import { Button } from "@/components/Button";
 import { CommentArea } from "@/components/CommentArea";
 import { HeaderSession } from "@/components/HeaderSession";
 import { LayoutAdmin } from "@/components/LayoutAdmin";
-import { signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-  const { status, data: session } = useSession();
 
   return (
     <LayoutAdmin>
