a0cac971500a61a6f939dd6bb58308c31b6d3370
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-01-25 20:17:26 -0300

    feat: created textarea component

---
+++ src/components/TextArea/index.tsx
@@ -0,0 +1,5 @@
+export function TextArea() {
+    return (
+        <textarea className="outline-none block w-full h-32 px-0 text-sm text-white bg-zinc-900 border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Envei um comentario com no maximo 250 caracteres..." />
+    );
+}
